e1b07a90f5b75f1ea0912cc9757a4fd05195e0a3
tuccinet <129888260+Tucci-net@users.noreply.github.com>
2024-03-20 00:46:41 -0700

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@
 // Get the <body>
 const lightDark = document.querySelector('a');
 const body = document.querySelector('body');
-
+let tl1 = gsap.timeline();
 
 // Manually add 'dark' class
 lightDark.addEventListener('click',()=> {
 body.classList.toggle('dark');
+tl1.from("main", {
+  opacity: 0,
+  ease: "bounce.inOut",
+  duration: 1.5
 
 });
+});
+
